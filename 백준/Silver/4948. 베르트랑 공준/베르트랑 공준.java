225a86ec3aad14f0889c775ac16132ac26b4a7d0
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2022-12-21 23:45:45 +0900

    [Silver II] Title: 베르트랑 공준, Time: 180 ms, Memory: 14844 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -6,6 +6,17 @@ public class Main {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int n = 123456;
+		boolean check[] = new boolean[n*2+1];
+		for(int i=2; i<=2*n; i++)
+			check[i]=true;
+		
+		for(int i=2; i<=Math.sqrt(2*n); i++) {
+			for(int j=i+i; j<=2*n; j+=i) {
+				check[j]=false;
+			}
+		}
+		
 		while(true) {
 			int N = Integer.parseInt(br.readLine());
 			int cnt=0;
@@ -14,21 +25,7 @@ public class Main {
 				break;
 			
 			for(int num=N+1; num<=2*N; num++) {
-				boolean flag = true;
-				
-	            //제곱근을 활용하여 구하기
-	            /*
-	            ex) 12의 약수 1,2,3,4,6,12 (1-12, 2-6, 3-4)이 짝을 이룬다.
-	            12의 제곱근 3.xx 즉, 제곱근까지만 확인해도 약수 존재를 확인할 수 있다.
-	            */
-				for(int j=2; j<=Math.sqrt(num); j++) {
-					if(num%j==0) {
-						flag=false;
-						break;
-					}
-				}
-				
-				if(flag)
+				if(check[num])
 					cnt++;
 			}
 			System.out.println(cnt);
