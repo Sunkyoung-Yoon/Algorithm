c713e805cec0babc3086178ebdca20f2d4a03a2e
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2022-12-22 23:30:36 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 496 ms, Memory: 18744 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -8,6 +8,21 @@ public class Main {
 		
 		int T = Integer.parseInt(br.readLine());
 		
+		int N = 10000;
+		boolean[] check = new boolean[N+1];
+		check[0]=true; //0은 소수아님
+		check[1]=true; //1은 소수아님
+		
+		for(int i=2; i<=Math.sqrt(N); i++) {
+			//이미 소수가 아니라고 판정된 수는 바로 다음 i로 넘어감
+			if(check[i]==true)
+				continue;
+			//false라면 그 수의 배수는 소수가 아님 ex) i=2, j=4,6,8..
+			for(int j=i+i; j<=N; j+=i) {
+				check[j]=true;
+			}
+		}
+		
 		for(int t=0; t<T; t++) {
 			int n = Integer.parseInt(br.readLine());
 			int a=0;
@@ -16,7 +31,7 @@ public class Main {
 			//i가 n/2에 가까워질수록 i와 n-i의 차는 작아짐
 			for(int i=1; i<=n/2; i++) {
 				//i와 n-i가 소수라면
-				if(check(i) && check(n-i)){
+				if(!check[i] && !check[n-i]){
 					a=i;
 					b=n-i;
 				}
@@ -24,15 +39,5 @@ public class Main {
 			System.out.println(a+" "+b);
 		}
 		
-	}
-	//소수 판별하는 메소드
-	public static boolean check(int num) {
-		for(int i=2; i<=Math.sqrt(num); i++) {
-			if(num%i==0) {
-				return false;
-			}
-				
-		}
-		return true;
 	}
 }
