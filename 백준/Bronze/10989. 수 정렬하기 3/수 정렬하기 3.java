977027b0d7209ee11ba927770c97c64d83603c93
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2022-12-27 10:47:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1704 ms, Memory: 357456 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
@@ -7,20 +6,29 @@ public class Main {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
 		
-		int cnt = Integer.parseInt(br.readLine());
-		int[] arr = new int[cnt];
+		int[] arr = new int[N];
+		int[] counting = new int[10001]; //배열은 0부터 시작하므로 +1
 		
-		for(int i=0; i<cnt; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i]=Integer.parseInt(br.readLine());
 		}
 		
-		Arrays.sort(arr);
-		
-		for(int i=0; i<cnt; i++) {
-			sb.append(arr[i]).append("\n");
+		//입력받은 수 등장횟수 카운트
+		for(int i=0; i<N; i++) {
+			counting[arr[i]]++;
 		}
 		
-		System.out.println(sb);
+		for(int i=0; i<10001; i++) {
+			if(counting[i]!=0) {
+				sb.append(i).append("\n");
+				counting[i]--; //출력후 등장횟수 -1
+				i--; //2번이상 등장한 경우 등장만큼 출력하기 위해 -1함
+			}
+		}
+		
+		System.out.print(sb);
+		
 	}
 }
