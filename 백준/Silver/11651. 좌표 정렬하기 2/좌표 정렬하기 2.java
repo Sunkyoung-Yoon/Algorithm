e23cd3693c6c51183ec69ba3219a6a83e1297d19
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2022-12-31 01:17:55 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 780 ms, Memory: 57968 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		
+		//x,y 저장
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		//정렬
+		//return 값이 양수면 자리변경 음수면 그대로
+		Arrays.sort(arr, (arr1, arr2) ->(arr1[1] == arr2[1])?arr1[0]- arr2[0]:arr1[1] - arr2[1]);
+		
+		for(int i=0; i<N; i++) {
+			sb.append(arr[i][0]+" "+arr[i][1]).append("\n");
+		}
+		
+		System.out.print(sb);
+		
+	}
+}
