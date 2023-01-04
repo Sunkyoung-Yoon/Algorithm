5daa62e12ac3dd2a1635bf411229d1f76407050b
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-01-05 03:03:40 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); //카드의 개수
+		int M = Integer.parseInt(st.nextToken()); //3장 카드의 합
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(blackjack(arr, N, M));
+	}
+	
+	public static int blackjack(int[] arr, int N, int M) {
+		int bestCase = 0;
+		
+		for(int i = 0; i<N-2; i++) {			
+			for(int j = i+1; j<N-1; j++) {
+				for(int k = j+1; k<N; k++) {
+					
+					int sum = arr[i]+arr[j]+arr[k];
+					if(sum>M) //3장 카드의 합이 M보다 큰 경우 제외
+						continue;
+					
+					//같다면 가장 최적의 경우이므로 반복문 종료
+					if(sum==M) {
+						bestCase=sum;
+						return bestCase;
+					}
+					//
+					bestCase = Math.max(bestCase, sum);
+				}
+			}
+		}
+		
+		return bestCase;
+	}
+	
+}
