7dd1a637508eea1a10fa33db6024a72d03ea0693
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-01-08 10:21:48 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 812 ms, Memory: 96704 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -11,14 +11,14 @@ public class Main {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		HashMap<Integer, String> int_map = new HashMap<>(); //key-포켓몬번호
 		HashMap<String, Integer> str_map = new HashMap<>(); //key-포켓몬이름
+		String[] str_arr = new String[N];
 		
 		//포켓몬이름,번호 문자열 해시맵에 저장
 		for(int i=1; i<=N; i++) {
 			String str = br.readLine();
-			int_map.put(i, str);
 			str_map.put(str, i);
+			str_arr[i-1] = str; 
 		}
 				
 		//M개의 문자열이 해시맵에 존재하는지 확인
@@ -27,7 +27,7 @@ public class Main {
 			
 			if(isInteger(str)) { //숫자라면
 				int num = Integer.parseInt(str);
-				sb.append(int_map.get(num)).append("\n");
+				sb.append(str_arr[num-1]).append("\n");
 			}
 			else { //문자열이라면
 				sb.append(str_map.get(str)).append("\n");
