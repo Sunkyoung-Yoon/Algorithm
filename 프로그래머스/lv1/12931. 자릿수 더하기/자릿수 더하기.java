1ccd5340db119feae42e917bc97018db7648ae08
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-01-16 21:22:16 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        
+        for(int i=0; i<str.length(); i++){
+            answer += str.charAt(i)-'0';
+        }
+        
+        return answer;
+    }
+}
