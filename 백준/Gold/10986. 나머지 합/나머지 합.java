d0fc826e0e9fbdd5c50c8773a87af4cb65878ae8
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-01-18 00:32:34 +0900

    [Gold III] Title: 나머지 합, Time: 500 ms, Memory: 112844 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -10,21 +10,21 @@ public class Main {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		long[] S = new long[N+1]; //합배열
+		long S = 0; //누적합을 저장할 변수
 		long[] count = new long[M]; //나머지별 등장 횟수  
 		long result = 0; //M으로 나누어 떨어지는 합 구간
 		
 		
 		//합 배열 저장
 		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++) {
-			S[i] = S[i-1]+Long.parseLong(st.nextToken());
+		for(int i=0; i<N; i++) {
+			S = S+Long.parseLong(st.nextToken());
 			
 			//배열 S1 값을 M으로 나눈 나머지 확인
-			if(S[i]%M==0) //나누어 떨어지는 구간
+			if(S%M==0) //나누어 떨어지는 구간
 				result++;
 			
-			count[(int)(S[i]%M)]++; //나머지별 등장 횟수 카운트
+			count[(int)(S%M)]++; //나머지별 등장 횟수 카운트
 		}
 
 		
