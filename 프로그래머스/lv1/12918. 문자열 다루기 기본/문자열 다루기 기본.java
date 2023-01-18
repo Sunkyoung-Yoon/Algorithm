c3bde83575dc3d27b57ecfbfd328ac952131db51
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-01-18 15:12:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = true;
-     
-        String str = s.replaceAll("[0-9]","0"); //숫자로만 이루어졌다면 모두 0으로 변경
         
-        if(!(str.length()==4 || str.length()==6)){
-            return false;
-        }
-        
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i)!='0'){
-                answer = false;
-                break;
+        if(s.length()==4 || s.length()==6){
+            try{
+                int n = Integer.parseInt(s);
+                return true;
+            }
+            catch(NumberFormatException e){
+                return false;
             }
         }
-        return answer;
+        else return false;
     }
 }
