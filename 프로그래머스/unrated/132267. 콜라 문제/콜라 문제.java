6cbc52a556f03afe541013a1dc95244897a8e1fc
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-01-21 19:15:31 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,14 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0; // 총 얻은 병수
-        int bottle = 0; //가게에
         
-        while(true){
-            if(n<a) break;
+        while(n>=a){
             answer += (n/a)*b; 
-            bottle = n%a; //가게에 주고 남은 병수
-            n = (n/a)*b; //얻은 병수
-            n += bottle; //총 갖고 있는 병수
+            n = (n/a)*b + n%a; //얻은 병수 + 가게에 주고 남은 병수
            
         }
         return answer;
