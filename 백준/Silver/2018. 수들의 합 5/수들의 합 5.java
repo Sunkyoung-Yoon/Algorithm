e29ee7731e5f1e6a7d7a14f493227dbd1707e5d7
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-01-23 22:32:47 +0900

    [Silver V] Title: 수들의 합 5, Time: 168 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -7,22 +7,28 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		int count=1; //가짓수 1개는 자기자신을 의미
 		
-		//예를 들어, 15면 7,8의 조합이 가장 큰 수들의 조합
-		//즉, N이 되는 연속된 수의 합은 N/2와 N/2+1 범위 이내가 된다.
-		for(int i=1; i<=N/2; i++) {
-			int sum=0;
-			for(int j=i; j<=(N/2)+1; j++) {
-				sum += j;
-				//i값이 포함될 때, N보다 커지는 경우가 생기면
-				//반복문 종료
-				if(sum>N)
-					break;
-				
-				//i값이 포함될 때 N이 되는 합을 찾았으므로 반복문 종료
-				if(sum==N) {
-					count++;
-					break;
-				}
+		//투포인터 풀이
+		int start = 0; //시작 포인터
+		int end = 0; //종료 포인터
+		int sum=0;
+		
+		while(end<N) {
+			//sum이 더 크다면 시작포인터 증가
+			if(sum>N) {
+				//합의 시작 부분이 바뀌었기 때문에 원래 시작한 수는 빼줌
+				sum-=start;
+				start++;
+			}
+			//sum이 작다면 종료포인터 증가
+			else if(sum<N){
+				end++;
+				//연속된 합의 마지막 수 새로 더함
+				sum+=end;
+			}
+			else { //sum==N이라면
+				count++;
+				end++;
+				sum+=end;
 			}
 		}
 		System.out.println(count);
