acf0090d3841d44428a98df173ebcfb933bab46d
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-02-11 05:26:45 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,17 +1,7 @@
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        //1000이하 중복없이 3개 최대 1000,999,998
-        boolean[] check = new boolean[2998];
-        
-        check[0] = true;
-        check[1] = true;
-        //소수가 아닌수 true
-        for(int i=2; i<=Math.sqrt(2998); i++){
-            for(int j=i+i; j<2998; j+=i){
-                check[j] = true;
-            }
-        }
+       
         //3개의 수 더한 수 소수 체크
         for(int i=0; i<nums.length-2; i++){
             for(int j=i+1; j<nums.length-1; j++){
@@ -19,11 +9,21 @@ class Solution {
                     
                     int sum = nums[i]+nums[j]+nums[k];
                     //소수라면
-                    if(!check[sum]) answer++;
+                    if(isPrime(sum)) answer++;
                 }
             }
         }
 
         return answer;
+    }
+    
+    //소수판별
+    public boolean isPrime(int num){
+        for(int i=2; i<=Math.sqrt(num); i++){
+            //1과 자기자신외에 나눠지는 수가 있다면 
+            if(num%i==0) return false;
+        }
+        
+        return true;
     }
 }
