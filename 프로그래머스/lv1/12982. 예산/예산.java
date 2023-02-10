fe6f130b9154dde146d1680be1e1faf0a3218689
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-02-11 02:12:48 +0900

    [level 1] Title: 예산, Time: 0.31 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -2,14 +2,15 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        Arrays.sort(d);
-        int sum = 0;
         
+        Arrays.sort(d);
         for(int i=0; i<d.length; i++){
-            sum+=d[i];
+            budget = budget-d[i];
             
-            if(sum>budget) break; //예산초과시 반복문 종료
-            else answer++; //예산 범위내이면 지원가능 부서 수 증가
+            //예산 초과되면 지원 종료
+            if(budget<0) break;
+            //초과안되면 부서 수 증가
+            else answer++;
         }
         return answer;
     }
