4cbede63d19f7bf1e6e7b6c00afb50189d3c1048
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-03-07 13:43:01 +0900

    [Gold V] Title: 거의 소수, Time: 260 ms, Memory: 26180 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		
+		//10의7승+1 크기의 배열 생성
+		boolean[] check = new boolean[10000001];
+		
+		//소수가 아닌수 true로 변경
+		check[0]=check[1]=true;
+		for(int i=2; i<=Math.sqrt(check.length-1); i++) {
+			//이미 소수가 아니라고 판별된 수는 continue
+			if(check[i]) continue;
+			
+			for(int j=i*i; j<=check.length-1; j+=i) {
+				check[j] = true;
+			}
+		}
+		
+		
+		int count = 0;
+		//소수리스트를 돌며 범위내에 거의소수 개수 확인
+		for(int i=2; i<=check.length-1; i++) {
+			
+			if(check[i]) continue;
+			
+			long result = i;
+			
+			//N제곱한 결과가 result*i <= B인 동안 반복
+			while(result <= B/i) {
+				//N제곱
+				result*=i;
+				
+				//A <= result <=B라면 거의 소수 존재 count증가
+				if(result>=A) count++;
+				
+			}
+		}	
+		
+		System.out.println(count);
+	}	
+}
