3452362c33b4e7821057ea90acd9178571239728
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-05-13 17:57:37 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,72 +4,69 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken()); 
+		int M = Integer.parseInt(st.nextToken());
 		char[][] board = new char[N][M];
-		String str = null;
 		
-		
-		//보드 색깔 배열에 저장
+		//MxN 크기 보드 저장
 		for(int i=0; i<N; i++) {
-			str=br.readLine(); //한 줄 저장 
+			String str = br.readLine();
 			for(int j=0; j<M; j++) {
-				board[i][j] = str.charAt(j); //한글자씩 끊어서 저장
+				board[i][j] = str.charAt(j);
 			}
 		}
 		
 		System.out.println(chess(board, N, M));
-		
 	}
 	
-	//다시 칠할 정사각형의 수 최솟값 구하는 함수
+	//8x8크기 체스판 중 다시 칠하는 횟수가 최소인 경우 찾는 함수
 	public static int chess(char[][] chess, int n, int m) {
+		int min = 64; //칠하는 횟수는 최대 64
 		
-		int result = 0;
-		int min = 64;
-		
-		//8x8으로 나누기
+		//8x8로 나누기
 		for(int i=0; i<n-8+1; i++) {
 			for(int j=0; j<m-8+1; j++) {
 				
-				//8x8 체스판 확인
-				int count = 0;
-				char check = chess[i][j]; //첫 시작 색깔
+				int count = 0; //나눈 체스판의 칠할 횟수
+				char check = chess[i][j]; //시작 색깔
 				
+				//체스판 시작점으로부터 8x8 크기 탐색
 				for(int k=0; k<8; k++) {
 					for(int p=0; p<8; p++) {
-						
-						//check와 다르면 색변경 필요 
+						//check와 다르다면 바꿔야하는 색이므로 count증가
 						if(chess[i+k][j+p]!=check) {
 							count++;
 						}
 						
-						//check 변경, 다음에는 다른 색이 와야함
-						if(check=='W')
+						//check 색 변경 W->B B->W
+						if(check=='W') {
 							check='B';
-						else
+						}
+						else {
 							check='W';
-							
+						}
 					}
-					//check 변경, 다음줄 처음에는 현재줄 맨끝과 같은색이 와야함
-					if(check=='W')
+					
+					//다음줄이 될 때는 이전 줄의 끝과 다음 줄의 시작이 같아야함
+					if(check=='W') {
 						check='B';
-					else
+					}
+					else {
 						check='W';
+					}
 				}
-			
-				count = Math.min(count, 64-count); //8x8 체스판 시작색에 맞춰서 구한 값과 시작색을 바꿔서 칠할 때 값 비교
-				min = Math.min(min, count); //다시 칠할 정사각형의 수 최솟값 확인
-				result = min; //최솟값 저장
+				
+				//8x8 체크판 변경횟수가 최솟값이라면 min 변경
+				//체스판의 맨 처음 시작 색이 반대일 경우도 고려해서 최소 count 저장
+				count = Math.min(count, 64-count);
+				min = Math.min(min, count);
 				
 			}
 		}
 		
-		return result;
+		return min;
 	}
-
 }
