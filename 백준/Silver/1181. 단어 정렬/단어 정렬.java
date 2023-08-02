e916997b8843a59eca54000d9796465e411a0aef
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-08-02 23:12:02 +0900

    [Silver V] Title: 단어 정렬, Time: 328 ms, Memory: 25760 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.*;
+
+//단어 정렬
 public class Main {
 
 	public static void main(String[] args) throws IOException{
@@ -15,17 +17,15 @@ public class Main {
 			str[i] = br.readLine();
 		}
 		
-		//Arrays.sort() 정렬조건 정의
+		//Arrays.sort() 정렬조건 재정의
 		Arrays.sort(str, (String o1, String o2)->o1.length()==o2.length()?o1.compareTo(o2):o1.length()-o2.length());
 		
-		sb.append(str[0]).append("\n"); //0번째 문자는 무조건 처음 등장한 단어
+		//0번째 문자는 무조건 처음 등장한 단어
+		sb.append(str[0]).append("\n");
 		//중복단어 제거
 		for(int i=1; i<N; i++) {
-			//현재 인덱스 위치 문자와 바로전 인덱스 위치의 문자가 같다면
-			if(str[i].equals(str[i-1]))
-				continue;
-			//같지 않다
-			else
+			//직전의 문자와 현재 문자가 다르다면 출력 대상
+			if(!str[i].equals(str[i-1]))
 				sb.append(str[i]).append("\n");
 		}
 		
