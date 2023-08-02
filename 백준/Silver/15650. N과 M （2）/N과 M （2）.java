ee67a128868f49e69528c2d4f6d5fced8965c0e2
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-08-03 01:39:09 +0900

    [Silver III] Title: N과 M (2), Time: 144 ms, Memory: 15844 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,54 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 // N과 M (2)
 public class Main {
-    public static int N, M;
-    public static int[] arr;
-    public static StringBuilder sb = new StringBuilder();
-    public static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-        visited = new boolean[N];
-
-        dfs(0, 1);
-        System.out.println(sb);
-
-    }
-
-    public static void dfs(int depth, int start) {
-        // M개가 되면 그 수열 sb에 저장
-        if(depth == M) {
-            for(int a: arr){
-                if(a == 0)
-                    continue;
-                sb.append(a+" ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        // 수열 찾기
-        for(int i=start; i<=N; i++) {
-            // 중복 허용x 방문하지않은 수로만 수열 찾기
-            if(!visited[i-1]) {
-                visited[i-1] = true;
-                arr[depth] = i;
-                // 수열을 만드는 반복문의 시작을 시작 수부터로 변경
-                dfs(depth + 1, i);
-
-                // 새로운 숫자로 수열을 시작하기 전에 방문표시 초기화
-                visited[i-1] = false;
-            }
-        }
-    }
+	static int N, M;
+	static int[] nums;
+	static boolean[] visited;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		nums = new int[N+1];
+		visited = new boolean[N+1];
+		sb = new StringBuilder();
+		
+		for(int i=1; i<=N; i++) {
+			nums[i] = i;
+		}
+		
+		result(0, 1);
+		
+		System.out.println(sb);
+		
+	}
+	
+	static void result(int cnt, int start) {
+		if(cnt == M) {
+			// 방문한 숫자들 -> 수열 -> sb에 저장
+			for(int i=1; i<=N; i++) {
+				if(visited[i] == true)
+					sb.append(nums[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		// 수열 만들기
+		for(int i=start; i<=N; i++) {
+			//이미 방문한 숫자는 패스
+			if(visited[i] == false) {
+				visited[i] = true;
+				result(cnt+1, i+1);
+				visited[i] = false;
+			}
+		}
+	}
 
 }
