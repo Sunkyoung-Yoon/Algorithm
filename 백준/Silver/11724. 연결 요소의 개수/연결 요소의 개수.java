8b149fd6fc41a28dc4e05cc308862e0b398b10e8
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-08-13 15:21:55 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 680 ms, Memory: 141900 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,45 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static ArrayList<Integer>[] arrayList; //그래프를 표현할 인접리스트
-	static boolean[] visited; //방문체크할 배열
-	
+	static ArrayList<Integer>[] arrayList;
+	static boolean[] visited;
 	public static void main(String[] args) throws IOException{
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		//정점(노드)의 개수
-		int N = Integer.parseInt(st.nextToken());
-		//간선의 개수
-		int M = Integer.parseInt(st.nextToken());
-		int count = 0; //연결 요소의 개수
-		arrayList = new ArrayList[N+1]; //정점은 1부터 시작하므로 N+1크기로 생성
+		int N = Integer.parseInt(st.nextToken()); //정점의 개수
+		int M = Integer.parseInt(st.nextToken()); //간선의 개수
+		arrayList = new ArrayList[N+1];
 		visited = new boolean[N+1];
+		int count = 0; // 연결 요소의 개수
 		
-		
-		//인접리스트의 ArrayList 각각 생성 및 초기화
-		for(int i=0; i<N+1; i++) {
-			arrayList[i] = new ArrayList<Integer>(); 
+		// 인접리스트 초기화 - 연결 정점의 번호가 1부터라 1로 시작
+		for(int i=1; i<=N; i++) {
+			arrayList[i] = new ArrayList<>();
 		}
-		
-		
-		//간선 정보(연결된 정점 정보) 저장
+				
+		// 간선 정보 저장
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			
-			//양방향으로 연결되어 있으므로 노드 양쪽 모두 저장
+			// 무방향 그래프 -> 양방향으로 저장
 			arrayList[u].add(v);
 			arrayList[v].add(u);
 		}
 		
-		//노드는 1번부터 시작
 		for(int i=1; i<=N; i++) {
-			//방문하지않은 노드라면 dfs 시작 - 연결된 노드들을 방문
-			//방문하지않은 노드라면 연결 요소의 개수 증가
+			// 아직 방문하지 않은 새로운 노드 발견시 탐색
 			if(!visited[i]) {
 				dfs(i);
 				count++;
@@ -50,15 +42,14 @@ public class Main {
 		
 	}
 	
-	public static void dfs(int now) {
-		visited[now] = true; //방문표시
+	static void dfs(int now) {
+		visited[now] = true;
 		
-		//인접한 노드 개수만큼 반복
-		for(int n : arrayList[now]) {
-			//방문하지 않은 인접노드 중에 방문
-			if(!visited[n]) {
+		// 인접한 노드 탐색
+		for(int n: arrayList[now]) {
+			if(!visited[n])
 				dfs(n);
-			}
 		}
 	}
+
 }
