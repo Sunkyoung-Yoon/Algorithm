0ea98dc8330edbd86c6cba3798be948f9115df49
Sunkyoung <97610532+Sunkyoung-Yoon@users.noreply.github.com>
2023-11-15 23:46:13 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 201 ms, Memory: 30,488 KB -BaekjoonHub

---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 응용] 4일차 - 보급로 - 1249 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15QRX6APsCFAYD) 
+
+### 성능 요약
+
+메모리: 30,488 KB, 시간: 201 ms, 코드길이: 2,092 Bytes
+
+### 제출 일자
+
+2023-11-15 23:46
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Solution {
+	static int N; 
+	static int[][] map;
+	static int[] di = {-1,1,0,0}; //상하좌우
+	static int[] dj = {0,0,-1,1};
+	static int minCost; 
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb= new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<=t; tc++) {
+			N = Integer.parseInt(br.readLine()); //지도크기 NxN
+			map = new int[N][N];
+			minCost = Integer.MAX_VALUE;
+			
+			String s = "";
+			for(int i=0; i<N; i++) {
+				s = br.readLine();
+				for(int j=0; j<N; j++) {
+					map[i][j] = s.charAt(j)-'0';
+				}
+			}
+			// print();
+			
+			bfs(0,0,0);
+			
+			sb.append("#"+tc+" "+minCost).append("\n");
+			
+			
+		} // 테스트케이스 종료
+		
+		System.out.println(sb);
+		
+	}
+	
+	static void bfs(int starti, int startj, int startCost) {
+		PriorityQueue<Point> pq = new PriorityQueue<>((o1, o2) ->{
+			// 비용(시간)이 작은 게 먼저
+			return o1.cost - o2.cost;
+		});
+		boolean[][] visited = new boolean[N][N];
+		pq.add(new Point(starti, startj, startCost));
+		visited[starti][startj] = true;
+		
+		while(!pq.isEmpty()) {
+			Point now = pq.poll();
+			//System.out.println("now방문:"+now.i+","+now.j+" "+now.cost);
+			
+			// 도착지 도착
+			if(now.i==N-1 && now.j==N-1) {
+				// 비용이 더 작은 경우발견했다면 업데이트
+				minCost = Math.min(minCost, now.cost);
+			}
+			
+			for(int d=0; d<4; d++) {
+				int nexti = now.i+di[d];
+				int nextj = now.j+dj[d];
+				
+				if(nexti>=0 && nexti<N && nextj>=0 && nextj<N
+						&& !visited[nexti][nextj]) {
+					int time = now.cost + map[nexti][nextj]; // 도달한 시간 저장
+					pq.add(new Point(nexti, nextj, time));
+					visited[nexti][nextj] = true;
+				}
+			}
+		}
+		
+		
+	}
+	
+	static class Point{
+		int i,j,cost;
+		
+		public Point(int i, int j, int cost) {
+			this.i = i;
+			this.j = j;
+			this.cost = cost;
+		}
+	}
+	
+	static void print(){
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				System.out.print(map[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
